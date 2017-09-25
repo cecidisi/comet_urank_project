1246a13e48120bc298fc2708307429124728bb41
cdisciascio <cdisciascio@know-center.at>
2017-09-25 21:12:33 +0200

    minor

--- scripts/settings-no-debug.py
+++ scripts/settings-no-debug.py
@@ -7,14 +7,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # os.path.join(BASE_DIR, ...)
 
 # Read in file
-with open('../comet_urank_project/settings.py', 'r') as file:
+with open('comet_urank_project/settings.py', 'r') as file:
 	filedata = file.read()
 
 # Replce target string
 filedata = filedata.replace('DEBUG = True', 'DEBUG = False')
 
 # Write file again
-with open('../comet_urank_project/settings.py', 'w') as file:
+with open('comet_urank_project/settings.py', 'w') as file:
 	file.write(filedata)
 
 print 'Set DEBUG = False in settings for production mode'
