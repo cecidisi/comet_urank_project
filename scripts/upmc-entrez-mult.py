a402b021d4d32cdece97364180113044a8253930
cdisciascio <cdisciascio@know-center.at>
2017-10-16 20:20:00 +0200

    improved fetching

--- scripts/upmc-entrez-mult.py
+++ scripts/upmc-entrez-mult.py
@@ -8,7 +8,7 @@ import unicodedata
 import datetime
 import time
 import multiprocessing as mp
-
+from django import db
 from progressbar import *
 from itertools import chain
 from bs4 import BeautifulSoup as bs
@@ -17,19 +17,20 @@ from conf_navigator.classes.bcolors import *
 from upmc_urank.models import *
 
 
-
 def run():
 	print 'RUN'
+	start = time.time()
 	clean_db()
 	term = 'migraine'
-	count = 10000 # Total number of items
+	count = 1000 # Total number of items
 	retmax = 100	# Number items per batch
-	cores = 10 #mp.cpu_count() -1
+	cores = max(10, int(mp.cpu_count() *.75))
 	base_url = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
 	query_key, web_env, max_count = search(base_url, term)
 	count = min(count, max_count) if count else max_count
 	# fetch_test(base_url, query_key, web_env, retmax, count, cores)
 	fetch_parallel(base_url, query_key, web_env, retmax, count, cores)
+	print_blue('Total Time Elapsed = ' + secToMMSS(time.time() - start))
 
 
 def clean_db():
@@ -62,9 +63,6 @@ def search(base_url, term):
 
 
 
-
-
-
 def fetch_worker(base_url, query_key, web_env, retmax, batches, i):
 	retstart = i * retmax
 	url_fetch = base_url + 'efetch.fcgi?db=pubmed&rettype=abstract&query_key=' + query_key + '&WebEnv=' + web_env + '&retstart=' + str(retstart) + '&retmax=' + str(retmax)
@@ -76,20 +74,25 @@ def fetch_worker(base_url, query_key, web_env, retmax, batches, i):
 		soup = bs(resp.text.encode('utf-8'), 'xml')
 		items = soup.find_all('PubmedArticle')
 		cur_attempt += 1
-	print 'Batch (' + str(i+1) + '/' + str(batches) + ') --> Got ' + str(len(items)) + ' items ('+str(cur_attempt-1)+' attempt/s) [retstart = ' + str(retstart) + ', retmax = ' + str(retmax) +']'
-	articles = []
+	print 'Batch (' + str(i+1) + '/' + str(batches) + ') --> Fetched ' + str(len(items)) + ' items ('+str(cur_attempt-1)+' attempt/s) [retstart = ' + str(retstart) + ', retmax = ' + str(retmax) +']'
+	paper_count = 0
+	# articles = []
 	for p in items:
-		paper = parse_paper(p)
-		articles.append(paper)
-	return articles
+		paper = save_paper(parse_paper(p))
+		if paper:
+			# articles.append(paper)
+			paper_count += 1
+	print 'Batch (' + str(i+1) + '/' + str(batches) + ') --> Saved ' +str(paper_count)+ ' items'
+	return paper_count
+	# return articles
 
 
 
 def fetch_parallel(base_url, query_key, web_env, retmax, count, cores):
-	print 'eFetching ...'
+	print 'eFetching with ' +str(cores)+ ' processes ...'
 	batches = count/retmax
 	jobs = []
-
+	db.connections.close_all()
 	tmsp1 = time.time()
 
 	worker = partial(fetch_worker, base_url, query_key, web_env, retmax, batches)
@@ -98,28 +101,34 @@ def fetch_parallel(base_url, query_key, web_env, retmax, count, cores):
 	pool.close()
 	pool.join()
 
-	tmsp2 = time.time()
-	# print_green('Time Lapse = ' + str(tmsp2) + ' sec.')
-	# jobs is list of lists
-	papers = list(chain.from_iterable(jobs))
-	print_green('Total papers fetched = ' + str(len(papers)) + ', Time elapsed = ' + str(tmsp2-tmsp1) + ' sec.')
 
-	# Set progressbar
-	widgets = ['Saving papers: ', Percentage(), ' ', Bar(marker='*',left='[',right=']'), ' ', ETA()]
-	pbar = ProgressBar(widgets=widgets, maxval=len(papers))
-	pbar.start()
-
-	saved_papers = 0
-	for i, d in enumerate(papers):
-		if save_paper(d):
-			saved_papers += 1
-		pbar.update(i)
-
-	pbar.finish()
-	tmsp3 = time.time() - tmsp1
-	print_green('Saved ' +str(saved_papers)+ ' papers. Total time elapsed --> ' + '{}:{}'.format(*divmod(int(tmsp3), 60)))
-
+	from functools import reduce
+	tmsp2 = time.time() - tmsp1
+	paper_count = str(reduce((lambda x, y: x+y), jobs))
+	# # jobs is list of lists
+	# papers = list(chain.from_iterable(jobs))
+	# paper_count = str(len(papers))
 	
+	print_green('Total papers =  ' + paper_count + '; eFetch Time = ' + secToMMSS(tmsp2))
+
+
+	# # Set progressbar
+	# widgets = ['Saving papers: ', Percentage(), ' ', Bar(marker='*',left='[',right=']'), ' ', ETA()]
+	# pbar = ProgressBar(widgets=widgets, maxval=len(papers))
+	# pbar.start()
+
+	# saved_papers = 0
+	# for i, d in enumerate(papers):
+	# 	if save_paper(d):
+	# 		saved_papers += 1
+	# 	pbar.update(i)
+
+	# pbar.finish()
+	# tmsp3 = time.time() - tmsp1
+	# print_green('Saved ' +str(saved_papers)+ ' papers. Total time elapsed --> ' + '{}:{}'.format(*divmod(int(tmsp3), 60)))
+
+def secToMMSS(sec):
+	return ':'.join([(str(i) if len(str(i))>1 else '0'+str(i)) for i in  divmod(int(sec), 60)])
 	
 
 '''
