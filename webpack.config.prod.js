4ab0c6212d7b72f7c80bc5a877eb5782780d574a
cdisciascio <cdisciascio@know-center.at>
2017-10-19 13:26:36 +0200

    fixed webpack config profuction

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -14,7 +14,7 @@ module.exports = {
     main_conf_nav: './assets/main_conf_nav',
     main_conf_nav_eval: './assets/main_conf_nav_eval',
     conf_nav_eval_general: './assets/conf_nav_eval_general',
-    main_upmc_urank: './assets/main_upmc_urank',
+    main_upmc: './assets/main_upmc',
   },
 
   output: {
